f335e610c939064341d31773b1376510861d51e0
thisisaciidfree <151845850+aciid1ree@users.noreply.github.com>
2024-04-15 10:17:11 +0300

    Delete test/Contracts/ApplicationsRequests.cs

--- test/Contracts/ApplicationsRequests.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace test.Contracts
-{
-    public record ApplicationsRequests (
-        Guid author,
-        string activity,
-        string name,  
-        string description, 
-        string outline);
-}
